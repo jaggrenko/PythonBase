a4a5918cc4c029445c513035486c062a44cf7fb5
jaggrenko <black_dog-82@mail.ru>
2020-11-26 21:20:00 +0300

    trash rm

--- HW1/1.py
+++
@@ -1,9 +0,0 @@
-user_inputs = []
-REQUEST_COUNT = 3
-
-for i in range(REQUEST_COUNT):
-    user_input = input('Уважаемый пользователь, введите число или строку')
-    user_inputs.append(user_input)
-
-print('Вы ввели следующие значения:')
-[print(val) for val in user_inputs]
--- HW1/4.py
+++
@@ -1,7 +0,0 @@
-while True:
-    user_input = input('Уважаемый пользователь, введите целое положительное число:')
-    if user_input.isdigit():
-        print(max(list(user_input)))
-        break
-    else:
-        print('Вы ввели неверное значение!')

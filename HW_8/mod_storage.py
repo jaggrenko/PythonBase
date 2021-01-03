2f1a14a8bacf121c8807a0fe1b0dc6db28e2d6a9
jaggrenko <black_dog-82@mail.ru>
2021-01-03 12:41:29 +0300

    Some exceptions optimized

--- HW_8/mod_storage.py
+++ HW_8/mod_storage.py
@@ -2,78 +2,105 @@ import shelve as sh
 
 
 class Storage:
+    # cls variables used as list indexes except @classmethod 'add_item()'
+    ind_new = 0
+    ind_attr = 1
+    ind_qnty = 2
+    ind_stor = -1
+
     @classmethod
     def add_item(cls, equip_dict):
-        for key, val in equip_dict.items():
-            with sh.open(f'{key}.md') as sh_obj:
-                if val[0] in sh_obj:  # or not in obj keys()
-                    tmp_lst = sh_obj[val[0]][:]
-                    tmp_lst.append(val[1:])
-                    sh_obj[val[0]] = tmp_lst
-                else:
-                    sh_obj[val[0]] = [val[1:]]
-        else:
-            cls._optimizer(key, val[0])
-            print(f'Добавлено: {val[3]} ед. техники [{key}:{val[0]}] --> склад')
+        # --> equip_dict.items structure: ('item_type': item_name, item_is_new, spec_attrib, item_quant, item_in_stor)
+        key, val = None, None
+        try:
+            for key, val in equip_dict.items():
+                with sh.open(f'{key}.md') as sh_obj:
+                    if val[0] in sh_obj:
+                        tmp_lst = sh_obj[val[0]][:]
+                        # {'item_name': (item_is_new, spec_attrib, item_quant, item_in_stor)} -->
+                        tmp_lst.append(val[1:])
+                        sh_obj[val[0]] = tmp_lst
+                    else:
+                        sh_obj[val[0]] = [val[1:]]
+            else:
+                cls._optimizer(key, val[0])
+                print(f'Добавлено: {val[3]} ед. техники [{key}:{val[0]}] --> склад')
+        except LookupError as err:
+            print(f'Ошибка (ключ [{val[0]}]): {err}')
+        except OSError as err:
+            print(f'Ошибка {err.errno}: {err.strerror}')
 
     @classmethod
     def rm_item(cls, item_type: str, item_name: str, item_is_new: bool, item_has_attr: bool, item_in_stor: bool,
                 cnt_to_rm: int):
         try:
             with sh.open(f'{item_type}.md') as sh_obj:
-                val = sh_obj[item_name][:]
-                for i, el in enumerate(val):
-                    match: bool = el[0] == item_is_new and el[1] == item_has_attr and el[-1] == item_in_stor
-                    if match and el[2] > 0:
-                        new_cnt = el[2] - cnt_to_rm
-                        if 0 <= new_cnt:
-                            val[i] = (el[0], el[1], new_cnt, el[-1])
-                            sh_obj[item_name] = val
-                            success_msg = f'Техника [{item_type}:{item_name}] с заданными параметрами в количестве {cnt_to_rm} ед. удалена <-- '
+                tmp_lst = sh_obj[item_name][:]
+                for i, el in enumerate(tmp_lst):
+                    match: bool = el[cls.ind_new] == item_is_new and el[cls.ind_attr] == item_has_attr \
+                                  and el[cls.ind_stor] == item_in_stor
+                    if match and el[cls.ind_qnty] > 0:
+                        new_cnt = el[cls.ind_qnty] - cnt_to_rm
+                        if new_cnt >= 0:
+                            tmp_lst[i] = (el[cls.ind_new], el[cls.ind_attr], new_cnt, el[cls.ind_stor])
+                            sh_obj[item_name] = tmp_lst
+                            success_msg = f'Техника [{item_type}:{item_name}] с заданными параметрами в количестве ' \
+                                          f'{cnt_to_rm} ед. удалена <-- '
                             print(f'{success_msg}склад') if item_in_stor else print(f'{success_msg}подразделения')
                             return True
                         else:
-                            print(f'Введите значение не больше {el[2]}')
+                            print(f'Введите значение не превышающее {el[cls.ind_qnty]}')
                             return False
-                    elif match and el[2] <= 0:
+                    elif match and el[cls.ind_qnty] <= 0:
                         print(f'Техника [{item_type}:{item_name}] с заданными параметрами отсутствует')
                         return False
-        except KeyError:
-            print(f'Значение с ключом [{item_type}:{item_name}] не найдено')
-            return False
+        except LookupError as err:
+            print(f'Ошибка [{item_type}:{item_name}]): {err}')
+        except OSError as err:
+            print(f'Ошибка {err.errno}: {err.strerror}')
 
     @classmethod
     def mv_item(cls, item_type: str, item_name: str, item_is_new: bool, item_has_attr: bool, item_in_stor: bool,
                 cnt_to_mv: int):
         if cls.rm_item(item_type, item_name, item_is_new, item_has_attr, item_in_stor, cnt_to_mv):
-            with sh.open(f'{item_type}.md') as sh_obj:
-                val: list = sh_obj[item_name][:]
-                for i, el in enumerate(val):
-                    match = el[0] == item_is_new and el[1] == item_has_attr and el[-1] == (not item_in_stor)
-                    if match:
-                        new_cnt = el[2] + cnt_to_mv
-                        val[i] = (el[0], el[1], new_cnt, el[-1])
-                        sh_obj[item_name] = val
-                        success_msg = f'Техника [{item_type}:{item_name}] с заданными параметрами в количестве {cnt_to_mv} ед. перемещена: '
-                        print(f'{success_msg}склад --> подразделения')if item_in_stor else print(f'{success_msg}подразделения --> склад')
+            try:
+                with sh.open(f'{item_type}.md') as sh_obj:
+                    tmp_lst = sh_obj[item_name][:]
+                    for i, el in enumerate(tmp_lst):
+                        match = el[cls.ind_new] == item_is_new and el[cls.ind_attr] == item_has_attr \
+                                and el[cls.ind_stor] == (not item_in_stor)
+                        if match:
+                            new_cnt = el[cls.ind_qnty] + cnt_to_mv
+                            tmp_lst[i] = (el[cls.ind_new], el[cls.ind_attr], new_cnt, el[cls.ind_stor])
+                            sh_obj[item_name] = tmp_lst
+                            success_msg = f'Техника [{item_type}:{item_name}] с заданными параметрами в количестве ' \
+                                          f'{cnt_to_mv} ед. перемещена: '
+                            print(f'{success_msg}склад --> подразделения') if item_in_stor \
+                                else print(f'{success_msg}подразделения --> склад')
+            except LookupError as err:
+                print(f'Ошибка [{item_type}:{item_name}]): {err}')
+            except OSError as err:
+                print(f'Ошибка {err.errno}: {err.strerror}')
 
     @classmethod
     def cnt_item(cls, item_type: str, item_name: str, item_in_stor: bool):
         try:
             with sh.open(f'{item_type}.md') as sh_obj:
-                val = sh_obj[item_name][:]
+                tmp_lst = sh_obj[item_name][:]
                 tmp_val_in = 0
                 tmp_val_out = 0
-                for el in val:
-                    if el[-1]:
-                        tmp_val_in += el[2]
+                for el in tmp_lst:
+                    if el[cls.ind_stor]:
+                        tmp_val_in += el[cls.ind_qnty]
                     else:
-                        tmp_val_out += el[2]
+                        tmp_val_out += el[cls.ind_qnty]
                 else:
                     print(f'Всего на складе [{item_type}:{item_name}]: {tmp_val_in} ед.') if item_in_stor \
                         else print(f'Передано подразделениям [{item_type}:{item_name}]: {tmp_val_out} ед.')
-        except KeyError:
-            print(f'Значение с ключом [{item_type}:{item_name}] не найдено')
+        except LookupError as err:
+            print(f'Ошибка [{item_type}:{item_name}]): {err}')
+        except OSError as err:
+            print(f'Ошибка {err.errno}: {err.strerror}')
 
     @classmethod
     def _optimizer(cls, item_type: str, item_name: str):
@@ -82,22 +109,28 @@ class Storage:
         states = [0, 0, 0, 0, 0, 0, 0, 0]
         mask = [(True, True, True), (True, True, False), (False, True, True), (False, True, False),
                 (True, False, True), (True, False, False), (False, False, True), (False, False, False)]
+        try:
+            with sh.open(f'{item_type}.md') as sh_obj:
+                tmp_lst = sh_obj[item_name][:]
+                for el in tmp_lst:
+                    tmp_tuple = tuple([el[cls.ind_new], el[cls.ind_attr], el[cls.ind_stor]])
+                    try:
+                        states[mask.index(tmp_tuple)] += el[cls.ind_qnty]
+                    except ValueError:
+                        continue
+                else:
+                    mask = list(map(list, mask))
+                    [mask[i].insert(2, states[i]) for i, val in enumerate(mask)]
+                    sh_obj[item_name] = list(map(tuple, mask))
+        except LookupError as err:
+            print(f'Ошибка [{item_type}:{item_name}]): {err}')
+        except OSError as err:
+            print(f'Ошибка {err.errno}: {err.strerror}')
 
-        with sh.open(f'{item_type}.md') as sh_obj:
-            val = sh_obj[item_name][:]
-            for el in val:
-                tmp_tuple = tuple([el[0], el[1], el[-1]])
-                try:
-                    states[mask.index(tmp_tuple)] += el[2]
-                except ValueError:
-                    continue
-            else:
-                mask = list(map(list, mask))
-                [mask[i].insert(2, states[i]) for i, val in enumerate(mask)]
-                sh_obj[item_name] = list(map(tuple, mask))
-
-    def sort_item(self):
-        pass
+    #TODO: cnt_item extended
+    # @classmethod
+    # def sort_item(self):
+    #     pass
 
 
 if __name__ == '__main__':
